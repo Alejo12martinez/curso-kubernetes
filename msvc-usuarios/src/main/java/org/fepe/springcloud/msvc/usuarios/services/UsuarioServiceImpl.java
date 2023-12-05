8fad51c0cdc6e861a07e522d3ed1bdd7464af54b
Alejo12martinez <alejo12martinez.g@gmail.com>
2023-12-04 23:01:45 -0500

    Curso Client Rest Actualización Curso Docker

--- msvc-usuarios/src/main/java/org/fepe/springcloud/msvc/usuarios/services/UsuarioServiceImpl.java
+++ msvc-usuarios/src/main/java/org/fepe/springcloud/msvc/usuarios/services/UsuarioServiceImpl.java
@@ -1,5 +1,6 @@
 package org.fepe.springcloud.msvc.usuarios.services;
 
+import org.fepe.springcloud.msvc.usuarios.clients.CursoClienteRest;
 import org.fepe.springcloud.msvc.usuarios.models.entity.Usuario;
 import org.fepe.springcloud.msvc.usuarios.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ import java.util.Optional;
 public class UsuarioServiceImpl implements UsuarioService{
     @Autowired
     private UsuarioRepository repository;
+
+    @Autowired
+    private CursoClienteRest client;
 
     @Override
     @Transactional(readOnly = true)
@@ -36,6 +40,7 @@ public class UsuarioServiceImpl implements UsuarioService{
     @Transactional
     public void eliminar(Long id) {
         repository.deleteById(id);
+        client.eliminarCursoUsuarioPorId(id);
     }
 
     @Override
